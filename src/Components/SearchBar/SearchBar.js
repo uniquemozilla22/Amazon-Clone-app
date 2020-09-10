7cac975fdf19315af2d78923e7b1409b65b93aac
Unique Bhattarai <uniqgaming26@gmail.com>
2020-09-10 12:01:40 +0545

    needed files created

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+
+    componentDidMount(){
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
