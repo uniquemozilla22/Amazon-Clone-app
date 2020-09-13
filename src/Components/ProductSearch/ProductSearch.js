50ff704168f496aaff1564e2c2e6d456b2268d00
Unique Bhattarai <uniqgaming26@gmail.com>
2020-09-13 21:48:06 +0545

    removed the bootsrap
    I removed the bootstrap file and used npm to install the file from the node package manager

--- src/Components/ProductSearch/ProductSearch.js
+++ src/Components/ProductSearch/ProductSearch.js
@@ -4,12 +4,12 @@ import React from 'react'
 const ProductSearch = () => {
     return (
         <div className="row">
-            <div className={'col-lg-4 col-md-4 col-sm-6 col-xs-12'}>
-                <img src="../../assets/img/product/hm2-pro-1.jpg" alt="product "/>
+            <div className='col-lg-4 col-md-4 col-sm-6 col-xs-12'>
+                <img src="../../assets/img/product/hm2-pro-1.jpg" alt="product"/>
             </div>
             <div className="col-lg-8 col-md-8 col-sm-6 col-xs-12">
-                <img src="../../assets/img/product/hm2-pro-1.jpg" alt="product "/>
-            </div>
+                <img src="../../assets/img/product/hm2-pro-1.jpg" alt="product"/>
+             </div>
         </div>
     );
 }
